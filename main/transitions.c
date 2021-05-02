459d958e630a23836db5a731b8b085daff7f65c1
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2021-05-02 13:46:16 +0200

    Implementing fsm for counting

--- main/transitions.c
+++ main/transitions.c
@@ -16,7 +16,40 @@
 #define IS_RISING_EDGE_GRIO_INTR_VAL(is_rising) ((is_rising) ? GPIO_INTR_POSEDGE : GPIO_INTR_NEGEDGE)
 #define WAS_RISING_EDGE_GRIO_INTR_VAL(was_rising) ((was_rising) ? GPIO_INTR_NEGEDGE : GPIO_INTR_POSEDGE)
 
+#define STATE_IDENTIFIER_LENGTH 4
+// Transitions
+#define T_0000_0001 1
+#define T_0000_0010 2
+#define T_0001_0100 20
+#define T_0001_0111 23
+#define T_0010_1011 43
+#define T_0010_1000 40
+#define T_0100_0001 65
+#define T_0100_0010 66
+#define T_0111_1110 126
+#define T_0111_1101 125
+#define T_1011_1110 190
+#define T_1011_1101 189
+#define T_1000_0001 129
+#define T_1000_0010 130
+#define T_1110_1011 235
+#define T_1110_1000 232
+#define T_1101_0100 212
+#define T_1101_0111 215
+
+// Impossible states
+#define S_IMP_0000 0 //only allowed as init state
+#define S_IMP_0011 3
+#define S_IMP_0101 5
+#define S_IMP_0110 6
+#define S_IMP_1001 9
+#define S_IMP_1010 10
+#define S_IMP_1111 15
+#define S_IMP_1100 12
+
 typedef uint8_t barrier_evt_q_item;
+// assuming that FSM state is defined by 4 bits
+typedef uint8_t fsm_transition;
 
 static barrier_evt_q_item FSM_STATE = 0;
 static const uint16_t BARRIER_EVT_Q_SIZE = 32;
@@ -31,6 +64,7 @@ static void outer_transition_timer_callback(TimerHandle_t);
 static void apply_state_change(barrier_evt_q_item);
 static void transition_handling_task(void *);
 static void initialize_null_handles();
+
 static void IRAM_ATTR inner_barrier_pin_isr(void *_)
 {
     static const barrier_evt_q_item state_change = INNER_BARRIER_FLAG;
@@ -45,6 +79,7 @@ static void IRAM_ATTR inner_barrier_pin_isr(void *_)
         gpio_isr_handler_add(INNER_BARRIER_PIN, inner_barrier_pin_isr, NULL);
     }
 }
+
 static void IRAM_ATTR outer_barrier_pin_isr(void *_)
 {
     static const barrier_evt_q_item state_change = OUTER_BARRIER_FLAG;
@@ -65,11 +100,13 @@ static void inner_transition_timer_callback(TimerHandle_t timer)
     gpio_set_intr_type(INNER_BARRIER_PIN, IS_RISING_EDGE_GRIO_INTR_VAL(is_inner_rising));
     gpio_isr_handler_add(INNER_BARRIER_PIN, inner_barrier_pin_isr, NULL);
 }
+
 static void outer_transition_timer_callback(TimerHandle_t timer)
 {
     gpio_set_intr_type(OUTER_BARRIER_PIN, IS_RISING_EDGE_GRIO_INTR_VAL(is_outer_rising));
     gpio_isr_handler_add(OUTER_BARRIER_PIN, outer_barrier_pin_isr, NULL);
 }
+
 static void apply_state_change(barrier_evt_q_item state_change)
 {
     barrier_evt_q_item prev_new_bits = FSM_STATE & FIRST_N_BITMASK(FLAG_COUNT);
@@ -78,6 +115,24 @@ static void apply_state_change(barrier_evt_q_item state_change)
     // XOR with changed state bits (1 for changed 0 for unchanged).
     FSM_STATE ^= prev_new_bits ^ (state_change & FIRST_N_BITMASK(FLAG_COUNT));
 }
+
+void detect_impossible_state(){
+	switch(FSM_STATE){
+	case S_IMP_0000: //call method only after first signal
+	case S_IMP_0011:
+	case S_IMP_0101:
+	case S_IMP_0110:
+	case S_IMP_1001:
+	case S_IMP_1010:
+	case S_IMP_1111:
+	case S_IMP_1100:
+		ESP_LOGE(TAG, "Inside impossible state: " BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(FSM_STATE));
+		break;
+	default:
+		break;
+	}
+}
+
 static void transition_handling_task(void *_)
 {
     barrier_evt_q_item state_change;
@@ -85,19 +140,20 @@ static void transition_handling_task(void *_)
     {
         while (xQueueReceive(barrier_evt_q, &state_change, portMAX_DELAY) != pdTRUE)
             ;
-        // TODO: check which pins have changed from "state_change" and change
-        // count depending on the FSM state transitions defined in the documents.
+        fsm_transition dangling_transition = FSM_STATE << STATE_IDENTIFIER_LENGTH;
         apply_state_change(state_change);
-        switch (state_change)
+        detect_impossible_state();
+        fsm_transition transition = dangling_transition | FSM_STATE;
+        switch(transition)
         {
-        case INNER_BARRIER_FLAG:
-            count = (count < MAX_ROOM_COUNT) ? count + 1 : count;
-            break;
-        case OUTER_BARRIER_FLAG:
-            count = (count > MIN_ROOM_COUNT) ? count - 1 : count;
-            break;
+        case T_0111_1110:
+			count = (count < MAX_ROOM_COUNT) ? count + 1 : count;
+		    break;
+        case T_1011_1101:
+			count = (count > MIN_ROOM_COUNT) ? count - 1 : count;
+			break;
         default:
-            break;
+        	break;
         }
         xQueueSend(count_display_q, (const void *)&count, portMAX_DELAY);
     }
